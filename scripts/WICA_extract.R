62de46a697e22dbd7d09ea6f07afe55aaf4d0741
acrunyon <amber.c.runyon@gmail.com>
2021-02-04 12:50:49 -0700

    scripts to load nClimGrid data and setup raster

---
+++ scripts/WICA_extract.R
@@ -0,0 +1,80 @@
+# How untar multiple files for NOAA data
+library(sf)
+library(raster)
+library(dplyr)
+library(rgdal)
+library(plyr)
+library(ggplot2)
+
+rm(list=ls())
+
+###############################################################################
+# Download NOAA data -- need to do this in separate script
+
+# Initials
+tar.dir <- "C:/Users/achildress/Documents/NOAA-data/" #location of tar.gz files
+data.dir <- paste(tar.dir,"data",sep="/") #location for text files
+
+# ### section to be filled in later downloading files to tar.dir
+
+# 
+# file_list<- list.files(path=tar.dir , pattern = "*.tar.gz", full.names = TRUE)
+# ldply(.data = file_list, .fun = untar, exdir = data.dir) #untar tarballs and save to data.dir
+
+###############################################################################
+
+
+# spatial data
+##############################################################################
+# initials
+site = "WICA"
+
+# read in parks shapefile
+nps_boundary <- st_read('C:/Users/achildress/OneDrive - DOI/Documents/GIS/nps_boundary2018/nps_boundary.shp')
+park <- filter(nps_boundary, UNIT_CODE == site) # subset to WRST only
+# park <- st_transform(park, st_crs(epsg))
+Sp_park <- as_Spatial(park)
+
+CRS  = crs(park) # get CRS
+bbox<-data.frame(Sp_park@bbox) # get bounding box
+
+# create stack for each var by pattern - "prcp.alaska" [AK not for whole period]
+pnt.list<-list.files(path=data.dir, pattern=".prcp.conus") #list all files by var
+
+# read in, create df, subset by max lat/lon
+df1 <- read.table(paste(data.dir,pnt.list[1],sep="/"))
+
+colnames(df1)=c("Lat","Lon","PrecipC")
+
+dfx<-subset(df1, Lat >= bbox["y","min"] & Lat <= bbox["y","max"] &
+              Lon >=bbox["x","min"] & Lon<=bbox["x","max"]) #doesn't work with small parks, need to fix
+
+#rasterize
+dfx<-as.matrix(dfx)
+e<-extent(dfx[,2:1])
+r <- raster(e, ncol=5, nrow=3)
+x <- rasterize(dfx[, 2:1], r, dfx[,3])
+plot(x)
+
+projection(x) = CRS("+init=epsg:4326")
+
+# Plot park over raster
+Sp_park<-spTransform(Sp_park,CRSobj = "+init=epsg:5070") #reproj sp obj; #  North America Albers Equal Area Conic
+
+# reproj raster
+X<-projectRaster(x,crs=crs(Sp_park))
+
+plot(X)
+plot(Sp_park,add=TRUE)
+
+# extract by polygon
+
+
+ggplot() + geom_raster(data = dfx, aes(x=Lon, y = Lat, fill=PrecipC)) 
+
+
+# extract single year
+data <- read.table(paste(data.dir,pnt.list,sep="/"))
+colnames(data)=c("Lat","Lon","PrecipC")
+
+
