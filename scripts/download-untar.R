8df34b5d220763bcc4f5b44d7cfc7a5e8a48d94e
Annie Kellner <annie.kellner@colostate.edu>
2021-02-18 06:32:59 -0700

    removed unnecessary packages

--- scripts/download-untar.R
+++ scripts/download-untar.R
@@ -1,11 +1,6 @@
 # nClimGrid batch download
 # ftp://ftp.ncdc.noaa.gov/pub/data/climgrid/
 
-library(sf)
-library(raster)
-library(dplyr)
-library(rgdal)
-library(plyr)
 library(HelpersMG)
 
 rm(list=ls())
@@ -17,10 +12,10 @@ tar.dir <- "D:/temp/" #location to save tar.gz files
 data.dir <- "D:/nClimGrid/" #location for text files
 
 # # downloading files to tar.dir -- commented out because already run
-# 
-# url.list<-paste(data.url,files.list[1:nrow(files.list),],sep="")
-# setwd(tar.dir)
-# wget(as.character(url.list, mode="wb"))
+
+url.list<-paste(data.url,files.list[1:nrow(files.list),],sep="")
+setwd(tar.dir)
+wget(as.character(url.list, mode="wb"))
 
  # Extract tarballs
 file_list<- list.files(path=tar.dir , pattern = "*.tar.gz", full.names = TRUE)
